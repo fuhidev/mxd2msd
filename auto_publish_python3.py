01c4705015a52d4d95848a050978da8f3d8e547e
Phuong Hieu Ho <fuhi.dev@gmail.com>
2024-03-06 17:34:04 +0700

    Create auto_publish_python3.py

---
+++ auto_publish_python3.py
@@ -0,0 +1,59 @@
+import arcpy
+import os
+import sys
+
+def read_mxd_in_folder(path):
+    print('-----------------------------------------')
+    print('dang doc du lieu tu folder')
+    files = []
+    # r=root, d=directories, f = files
+    for r, d, f in os.walk(path):
+        for file in f:
+            if '.mxd' in file:
+                files.append(os.path.join(r, file))
+    print('-------------------//----------------------')
+    return files
+
+def set_datasource(mxd_path, database):
+    try:
+        print('-----------------------------------------')
+        print('set data source ' + mxd_path)
+        mxd = arcpy.mapping.MapDocument(mxd_path)
+        resource_layers = arcpy.mapping.ListLayers(mxd)
+
+        for layer in resource_layers:
+            if layer.supports("DATASOURCE"):
+                layer.replaceDataSource(database, 'SDE_WORKSPACE')
+        mxd.save()
+        del mxd
+        return True
+    except Exception as e:
+        print('khong the set datasource ' + mxd_path)
+        print(e)
+    print('-------------------//----------------------')
+    return False
+
+def mxd_to_msd(mxd_path):
+    try:
+        print('-----------------------------------------')
+        print('mxd to msd ' + mxd_path)
+        mxd = arcpy.mapping.MapDocument(mxd_path)
+        msd_path = os.path.splitext(mxd_path)[0]
+        arcpy.mapping.ConvertToMSD(mxd, msd_path)
+        del mxd
+    except Exception as e:
+        print('khong the chuyen mxd sang msd ' + mxd_path)
+        print(e)
+    print('-------------------//----------------------')
+
+if __name__ == "__main__":
+    folder = os.getcwd()
+    database_path = os.path.join(folder, 'db.sde')
+    if not os.path.exists(folder):
+        raise Exception(folder + ' khong ton tai')
+    if not os.path.exists(database_path):
+        raise Exception(database_path + ' khong ton tai')
+    files = read_mxd_in_folder(folder)
+    for file in files:
+        if set_datasource(file, database_path):
+            mxd_to_msd(file)
